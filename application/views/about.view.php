5fa8511e10eeb361f552b0206bf3b36af44d74e3
Nils Martinussen <nilsma231@gmail.com>
2014-06-17 15:01:10 +0200

    fixed broken link

--- application/views/about.view.php
+++ application/views/about.view.php
@@ -30,7 +30,7 @@ if(!class_exists('About_View')) {
             $html .= 'and I cannot guarantee uptime, backups, or longevity of the project.</p>' . "\n";
             $html .= '<p>So you would probably not want to rely on Lister as a permanent storage of ';
             $html .= 'your lists - not yet anyway</p>' . "\n";
-            $html .= '<p>Any feedback about the application\'s <a href="#">code</a> in particular, ';
+            $html .= '<p>Any feedback about the application\'s <a href="https://github.com/nilsma/mvc-lister">code</a> in particular, ';
             $html .= 'or anything else, is most welcome at <a id="email" href="mailto:lister%40nima-design%2enet?subject=Web Design" title="Send me an email">lister@nima-design.net</a></p>';
             $html .= '<p><a href="register.php">Register</a> or <a href="login.php">login</a> to start using Lister.</p>' . "\n";
             $html .= '</div>' . "\n";
